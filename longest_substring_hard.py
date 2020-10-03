836bef61fa60068fda5f9a83bd3d46e4a973a2fa
Ricky <rickdhanota@gmail.com>
2020-10-03 16:58:33 -0700

    solved the algo

--- longest_substring_hard.py
+++ longest_substring_hard.py
@@ -4,6 +4,15 @@
 #Sample output, string = "mentisac"
 
 def longestSubstringWithoutDuplication(string):
-    pass
+    lastSeen = {}
+    longest = [0, 1]
+    startIdx = 0
+    for i, char in enumerate(string):
+        if char in lastSeen:
+            startIdx = max(startIdx, lastSeen[char] + 1)
+        if longest[1] - longest[0] < i + 1 - startIdx:
+            longest = [startIdx, i+1]
+        lastSeen[char] = i
+    return string[longest[0] : longest[1]] #the longest[1] is excluded in python        
 
 print(longestSubstringWithoutDuplication("clementisacap"))
