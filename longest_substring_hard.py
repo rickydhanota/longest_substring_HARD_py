05bc8288686408012183813610371b1aaed9e045
Ricky <rickdhanota@gmail.com>
2020-10-03 15:33:36 -0700

    added problem statement

---
+++ longest_substring_hard.py
@@ -0,0 +1,9 @@
+# Write a function that takes in a string and returns its longest substring withpout duplicate characters. You can assume that there will only be one longest substring without duplication
+
+#Sample input, string = "clementisacap"
+#Sample output, string = "mentisac"
+
+def longestSubstringWithoutDuplication(string):
+    pass
+
+print(longestSubstringWithoutDuplication("clementisacap"))
